aa7297ade864a7d6435bf3a006b191e89d89502b
Justin Dahmubed <jdahmubed@jdahmubed-macbookpro.roam.corp.google.com>
2018-04-26 15:46:22 -0700

    PR changes

--- src/org/oidc/service/AbstractService.java
+++ src/org/oidc/service/AbstractService.java
@@ -1,4 +1,3 @@
-/*
 package org.oidc.service;
 
 import com.auth0.msg.Message;
@@ -13,6 +12,7 @@ import org.oidc.common.ResponseException;
 import org.oidc.common.SerializationType;
 import org.oidc.common.ServiceName;
 import org.oidc.common.UnsupportedSerializationTypeException;
+import org.oidc.message.Message;
 import org.oidc.service.base.HttpArguments;
 import org.oidc.service.base.HttpHeader;
 import org.oidc.service.base.ServiceConfig;
@@ -22,160 +22,120 @@ import org.oidc.service.util.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-*/
 /**
  * This is the base class for all services and provides default implementation for various methods.
- *//*
-
+ */
 public abstract class AbstractService implements Service {
 
-    */
-/**
+    /**
      * Message that describes the request.
-     *//*
-
+     */
     protected Message requestMessage;
 
-    */
-/**
+    /**
      * Message that describes the response.
-     *//*
-
+     */
     protected Message responseMessage;
 
-    */
-/**
+    /**
      * The name used for the endpoint in provider information discovery
-     *//*
-
+     */
     protected EndpointName endpointName;
 
-    */
-/**
+    /**
      * True if the response will be returned as a direct response to the request.
      * The only exception right now to this is the Authorization request where the
      * response is delivered to the client at some later date. Default is True
-     *//*
-
+     */
     protected boolean isSynchronous = true;
 
-    */
-/**
+    /**
      * ServiceName - enum (A name of the service. Later when a RP/client is
      * implemented instances of different services are found by using this name.
      * No default)
-     *//*
-
+     */
     protected ServiceName serviceName;
 
-    */
-/**
+    /**
      * Client authentication method - defined in enum ClientAuthenticationMethod
      * (The client authentication method to use if nothing else is specified.
      * Default is '' which means none.)
-     *//*
-
+     */
     protected ClientAuthenticationMethod defaultAuthenticationMethod = ClientAuthenticationMethod.NONE;
 
-    */
-/**
+    /**
      * HttpMethod - enum (Which HTTP method to use when sending the request. Default
      * is GET)
-     *//*
-
+     */
     protected HttpMethod httpMethod = HttpMethod.GET;
 
-    */
-/**
+    /**
      * SerializationType - enum (The serialization method to be used for the
      * request. Default is urlencoded)
-     *//*
-
+     */
     protected SerializationType serializationType = SerializationType.URL_ENCODED;
 
-    */
-/**
+    /**
      * The deserialization method to use on the response. Default is json
-     *//*
-
+     */
     protected SerializationType deserializationType = SerializationType.JSON;
 
-    */
-/**
+    /**
      * Additional configuration arguments that could be used to change default values
      * like ClientAuthenticationMethod or add extra parameters to pre/postConstruct methods.
-     *//*
-
+     */
     protected ServiceConfig config;
 
-    */
-/**
+    /**
      * The actual URL provided in provider information discovery.
-     *//*
-
+     */
     private String endpoint = "";
 
-    */
-/**
+    /**
      * Serves as an in-memory cache
-     *//*
-
+     */
     protected State state;
 
-    */
-/**
+    /**
      * It contains information that a client needs to talk to a server. This is shared by various services.
-     *//*
-
+     */
     protected ServiceContext serviceContext;
 
-    */
-/**
+    /**
      * Arguments to be used by the preConstruct methods
-     *//*
-
+     */
     private Map<String,String> preConstruct;
 
-    */
-/**
+    /**
      * Arguments to be used by the postConstruct methods
-     *//*
-
+     */
     private Map<String,String> postConstruct;
 
-    */
-/**
+    /**
      * Configuration that is specific to every service
-     *//*
-
+     */
     protected ServiceConfig serviceConfig;
 
-    */
-/**
+    /**
      * Additional claims
-     *//*
-
+     */
     private AddedClaims addedClaims;
 
-    */
-/**
+    /**
      * Constants
-     *//*
-
+     */
     private static final String HTTP_METHOD = "httpMethod";
     private static final String AUTHENTICATION_METHOD = "authenticationMethod";
     private static final String SERIALIZATION_TYPE = "serializationType";
 
     private static final Logger logger = LoggerFactory.getLogger(AbstractService.class);
 
-    */
-/**
+    /**
      * @param serviceContext It contains information that a client needs to talk to a server.
      *                       This is shared by various services.
      * @param state Serves as an in-memory cache
      * @param config Configuration that is specific to every service
-     *//*
-
+     */
     public AbstractService(ServiceContext serviceContext,
                            State state,
                            ServiceConfig config,
@@ -186,8 +146,7 @@ public abstract class AbstractService implements Service {
         this.addedClaims = addedClaims;
     }
 
-    */
-/**
+    /**
      This method will run after the response has been parsed and verified.  It requires response and
      stateKey in order for the service context to be updated.  StateKey is used to fetch and update
      the appropriate State associated with a specific service. This method may update certain attributes
@@ -195,26 +154,22 @@ public abstract class AbstractService implements Service {
 
      * @param response the response as a Message instance
      * @param stateKey the key that identifies the State object
-     **//*
-
+     **/
     public abstract void updateServiceContext(
             Message response,
             String stateKey);
 
-    */
-/**
+    /**
      * This method will run after the response has been parsed and verified.  It requires response
      * in order for the service context to be updated.  This method may update certain attributes
      * of the service context such as issuer, clientId, or clientSecret.  This method does not require
      * a stateKey since it is used for services that are not expected to store state in the state DB.
 
      * @param response the response as a Message instance
-     *//*
-
+     */
     public abstract void updateServiceContext(Message response);
 
-    */
-/**
+    /**
      This the start of a pipeline that will:
 
      - Deserializes a response into its response message class.
@@ -224,8 +179,7 @@ public abstract class AbstractService implements Service {
      * @param serializationType  which serialization that was used
      * @param stateKey the key that identifies the State object
      * @return the parsed and to some extent verified response
-     **//*
-
+     **/
     public Message parseResponse(String responseBody, SerializationType serializationType, String stateKey) throws Exception {
         if(serializationType == null) {
             serializationType = this.serializationType;
@@ -237,7 +191,6 @@ public abstract class AbstractService implements Service {
         }
 
         Message response = null;
-
         try {
             if(SerializationType.URL_ENCODED.equals(serializationType)) {
                 response = this.responseMessage.fromUrlEncoded(urlInfo);
@@ -268,7 +221,7 @@ public abstract class AbstractService implements Service {
             boolean isSuccessful;
             try {
                 //todo: how can I add the claims above? Leo
-                isSuccessful = response.verify(addedClaims);
+                isSuccessful = response.verify();
             } catch (Exception e) {
                 logger.error("Exception while verifying response");
                 throw e;
@@ -280,11 +233,10 @@ public abstract class AbstractService implements Service {
             }
 
             //TODO
-            */
-/*if(response instanceof AuthorizationResponse && Strings.isNullOrEmpty(response.getScope())) {
+            /*
+            if(response instanceof AuthorizationResponse && Strings.isNullOrEmpty(response.getScope())) {
                 response.setScope(addedClaims.getScope());
-            }*//*
-
+            }*/
 
             response = this.postParseResponse(response, stateKey);
         }
@@ -296,13 +248,11 @@ public abstract class AbstractService implements Service {
         return response;
     }
 
-    public Message postParseResponse(Message response, String stateKey) {
-
-        return response;
+    public Message postParseResponse(Message responseMessage, String stateKey) {
+        return null;
     }
 
-    */
-/**
+    /**
      This the start of a pipeline that will:
 
      - Deserializes a response into its response message class.
@@ -310,14 +260,12 @@ public abstract class AbstractService implements Service {
      verify method belonging to the message class used.
      * @param responseBody The response, can be either in a JSON or an urlencoded format
      * @return the parsed and to some extent verified response
-     **//*
-
+     **/
     public Message parseResponse(String responseBody) throws Exception {
         return parseResponse(responseBody, SerializationType.JSON, "");
     }
 
-    */
-/**
+    /**
      This the start of a pipeline that will:
 
      - Deserialize a response into its message class.
@@ -326,18 +274,16 @@ public abstract class AbstractService implements Service {
      * @param responseBody The response, can be either in a JSON or an urlencoded format
      * @param serializationType  which serialization that was used
      * @return the parsed and to some extent verified response
-     **//*
-
+     **/
     public Message parseResponse(
             String responseBody, SerializationType serializationType) throws Exception {
         return parseResponse(responseBody, serializationType, "");
     }
 
-    */
-/**
+    /**
      * Builds the request message and constructs the HTTP headers.
 
-     This is the starting point for a pipeline that will:
+     This is the starting pont for a pipeline that will:
 
      - construct the request message
      - add/remove information to/from the request message in the way a
@@ -347,8 +293,7 @@ public abstract class AbstractService implements Service {
      urlencoded, signed JWT)
      * @param requestArguments
      * @return HttpArguments
-     *//*
-
+     */
     public HttpArguments getRequestParameters(Map<String,String> requestArguments) throws UnsupportedSerializationTypeException {
         if(requestArguments == null) {
             throw new IllegalArgumentException("null requestArguments");
@@ -396,9 +341,147 @@ public abstract class AbstractService implements Service {
         return httpArguments;
     }
 
-    //todo: default implementation is that service will instatiate specific request
-    //this is the method where pre/postConstruct will be applied based on a service type
-    public abstract Message constructRequest();
+    public Message constructRequest() {
+        return null;
+    }
 
+    public Message getRequestMessage() {
+        return requestMessage;
+    }
 
-}*/
+    public void setRequestMessage(Message requestMessage) {
+        this.requestMessage = requestMessage;
+    }
+
+    public Message getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(Message responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public EndpointName getEndpointName() {
+        return endpointName;
+    }
+
+    public void setEndpointName(EndpointName endpointName) {
+        this.endpointName = endpointName;
+    }
+
+    public boolean isSynchronous() {
+        return isSynchronous;
+    }
+
+    public void setSynchronous(boolean synchronous) {
+        isSynchronous = synchronous;
+    }
+
+    public ServiceName getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(ServiceName serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public ClientAuthenticationMethod getDefaultAuthenticationMethod() {
+        return defaultAuthenticationMethod;
+    }
+
+    public void setDefaultAuthenticationMethod(ClientAuthenticationMethod defaultAuthenticationMethod) {
+        this.defaultAuthenticationMethod = defaultAuthenticationMethod;
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public static String getAuthenticationMethod() {
+        return AUTHENTICATION_METHOD;
+    }
+
+    public void setHttpMethod(HttpMethod httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public SerializationType getSerializationType() {
+        return serializationType;
+    }
+
+    public void setSerializationType(SerializationType serializationType) {
+        this.serializationType = serializationType;
+    }
+
+    public SerializationType getDeserializationType() {
+        return deserializationType;
+    }
+
+    public void setDeserializationType(SerializationType deserializationType) {
+        this.deserializationType = deserializationType;
+    }
+
+    public ServiceConfig getConfig() {
+        return config;
+    }
+
+    public void setConfig(ServiceConfig config) {
+        this.config = config;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public ServiceContext getServiceContext() {
+        return serviceContext;
+    }
+
+    public void setServiceContext(ServiceContext serviceContext) {
+        this.serviceContext = serviceContext;
+    }
+
+    public Map<String, String> getPreConstruct() {
+        return preConstruct;
+    }
+
+    public void setPreConstruct(Map<String, String> preConstruct) {
+        this.preConstruct = preConstruct;
+    }
+
+    public Map<String, String> getPostConstruct() {
+        return postConstruct;
+    }
+
+    public void setPostConstruct(Map<String, String> postConstruct) {
+        this.postConstruct = postConstruct;
+    }
+
+    public ServiceConfig getServiceConfig() {
+        return serviceConfig;
+    }
+
+    public void setServiceConfig(ServiceConfig serviceConfig) {
+        this.serviceConfig = serviceConfig;
+    }
+
+    public AddedClaims getAddedClaims() {
+        return addedClaims;
+    }
+
+    public void setAddedClaims(AddedClaims addedClaims) {
+        this.addedClaims = addedClaims;
+    }
+}
--- src/org/oidc/service/base/ServiceContext.java
+++ src/org/oidc/service/base/ServiceContext.java
@@ -1,4 +1,3 @@
-/*
 package org.oidc.service.base;
 
 import com.auth0.msg.DataLocation;
@@ -18,57 +17,42 @@ import org.oidc.message.ProviderConfigurationResponse;
 import org.oidc.message.RegistrationRequest;
 import org.oidc.message.RegistrationResponse;
 
-*/
 /**
  This class keeps information that a client needs to be able to talk
  to a server. Some of this information comes from configuration and some
  from dynamic provider info discovery or client registration, but information is also
  picked up during the conversation with a server.
- **//*
-
+ **/
 public class ServiceContext {
-    */
-/**
+    /**
      * Used to store keys
-     *//*
-
+     */
     private KeyJar keyJar = new KeyJar();
-    */
-/**
+    /**
      * Additional configuration arguments
-     *//*
-
+     */
     private ServiceContextConfig config;
-    */
-/**
+    /**
      * Where dynamically received or statically assigned provider
      * information is stored
-     *//*
-
+     */
     private ProviderConfigurationResponse providerConfigurationResponse;
-    */
-/**
+    /**
      * Where the response to a dynamic client registration is stored.
-     *//*
-
+     */
     private RegistrationResponse registrationResponse;
-    */
-/**
+    /**
      * A client will need to dynamically construct a number of URLs, which
      * is the basis for all the URLs.
-     *//*
-
+     */
     private String baseUrl;
-    */
-/**
+    /**
      * Doing Authorization request parts or the whole request can be
      * passed by reference using the requestUri. The request itself
      * must be stored somewhere hence the requestsDirectory.
-     *//*
-
+     */
     private String requestsDirectory;
-    */
-/**
+    /**
      * Divergence from the standard can be more or less severe.
      * Less severe cases can be allowed but only if it’s deemed OK.
      * The only example of this right now is issuer mismatch.
@@ -81,53 +65,39 @@ public class ServiceContext {
      * Some OP implementations doesn’t live up to this.
      * And that by design. So, if you want to talk to those you have to
      * allow them to diverge from the standard.
-     *//*
-
+     */
     private Map<String,Boolean> allow;
-    */
-/**
+    /**
      * If manual client registration is done, this is where the results of
      * that is kept.
      * If dynamic client registration is done, this is the result of
      * mapping the registration response against the clientPreferences
-     *//*
-
+     */
     private RegistrationResponse behavior;
-    */
-/**
+    /**
      * If dynamic client registration is done, this is where it’s specified
      * what the client would like to use. This is the basis for the
      * registration request.
-     *//*
-
+     */
     private RegistrationRequest clientPreferences;
-    */
-/**
+    /**
      * The client identifier, which is always required.
-     *//*
-
+     */
     private String clientId;
-    */
-/**
+    /**
      * The client secret, which is optional.
-     *//*
-
+     */
     private String clientSecret;
-    */
-/**
+    /**
      * The Issuer ID. This is the unique identifier of the OP/AS the
      * client is communicating with.
-     *//*
-
+     */
     private String issuer;
-    */
-/**
+    /**
      * An unordered list of redirect uris that the client expects to use.
-     *//*
-
+     */
     private List<String> redirectUris;
-    */
-/**
+    /**
      * redirectUris contains a list of unspecified redirect uris.
      * In reality there are good reasons for having separated redirect
      * uris for different expected response modes.
@@ -135,8 +105,7 @@ public class ServiceContext {
      * fragment, queryString, formPost
      * In callback, we can keep the redirect uris per response mode
      * separate.
-     *//*
-
+     */
     private Map<DataLocation,String> callBack;
 
     public ServiceContext(
@@ -150,12 +119,10 @@ public class ServiceContext {
 
     }
 
-    */
-/**
+    /**
      The client needs its own set of keys. It can either dynamically create them or load them from local storage. This method can also fetch other entities keys provided that the URL points to a JWKS.
      * @param keySpecifications contains fileName and algorithm
-     **//*
-
+     **/
     public void importKeys(Map<FileOrUrl,KeySpecifications> keySpecifications) {
         if(keySpecifications == null) {
             throw new IllegalArgumentException("null keySpecifications");
@@ -180,8 +147,7 @@ public class ServiceContext {
         }
     }
 
-    */
-/**
+    /**
      A 1<->1 map is maintained between a URL pointing to a file and
      the name of the file in the file system.
 
@@ -191,8 +157,7 @@ public class ServiceContext {
      Relative to the directory from which the RP instance is run.
      * @param webName the published URL
      * @return local filename
-     **//*
-
+     **/
     public String fileNameFromWebname(String webName) throws ValueException {
         if(Strings.isNullOrEmpty(webName)) {
             throw new IllegalArgumentException("null or empty webName");
@@ -210,14 +175,12 @@ public class ServiceContext {
         }
     }
 
-    */
-/**
+    /**
      Need to generate a redirectUri path that is unique for an OP/RP combo.  This is to counter the mix-up attack.
 
      * @param requestsDirectory the leading path
      * @return a list of one unique URL
-     **//*
-
+     **/
     public List<String> generateRequestUris(String requestsDirectory) throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         if(!Strings.isNullOrEmpty(this.providerConfigurationResponse.getIssuer())) {
@@ -257,4 +220,3 @@ public class ServiceContext {
         return allow;
     }
 }
-*/
