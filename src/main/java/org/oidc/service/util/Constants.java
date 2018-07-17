4a9f5bf59d9ad1561190f49a2c8213dbf5daf46b
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-07-17 18:41:11 +0300

    Added OIDCONF_PATTERN

--- src/main/java/org/oidc/service/util/Constants.java
+++ src/main/java/org/oidc/service/util/Constants.java
@@ -3,6 +3,7 @@ package org.oidc.service.util;
 public class Constants {
   public static final String OIDC_ISSUER = "http://openid.net/specs/connect/1.0/issuer";
   public static final String WEB_FINGER_URL = "https://%s/.well-known/webfinger";
+  public static final String OIDCONF_PATTERN = "%s/.well-known/openid-configuration";
 
   /**
    * Claims
