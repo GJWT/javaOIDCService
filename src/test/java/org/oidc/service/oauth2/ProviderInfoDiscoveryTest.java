0f0821d2f1c6f520f4ec8a6fcc5bb28f88dd7b5c
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-07-19 20:47:11 +0300

    Improved testing

--- src/test/java/org/oidc/service/oauth2/ProviderInfoDiscoveryTest.java
+++ src/test/java/org/oidc/service/oauth2/ProviderInfoDiscoveryTest.java
@@ -1,14 +1,19 @@
 package org.oidc.service.oauth2;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.oidc.common.HttpMethod;
 import org.oidc.common.MissingRequiredAttributeException;
+import org.oidc.msg.InvalidClaimException;
+import org.oidc.msg.oauth2.ASConfigurationResponse;
 import org.oidc.service.base.HttpArguments;
 import org.oidc.service.base.ServiceContext;
+import org.oidc.service.util.Constants;
 
 /**
  * Unit tests for {@link ProviderInfoDiscovery}
@@ -25,16 +30,15 @@ public class ProviderInfoDiscoveryTest {
   }
 
   @Test(expected = MissingRequiredAttributeException.class)
-  public void testMissingIssuer() throws Exception {
+  public void testHttpParamsMissingIssuer() throws Exception {
     ProviderInfoDiscovery service = new ProviderInfoDiscovery(serviceContext, null, null);
     service.getRequestParameters(new HashMap<String, String>());
   }
 
   @Test
-  public void testSuccessfulIssuer() throws Exception {
+  public void testHttpParamsSuccessfulIssuer() throws Exception {
     serviceContext.setIssuer(issuer);
     ProviderInfoDiscovery service = new ProviderInfoDiscovery(serviceContext, null, null);
-    System.out.println(service.getServiceContext().getIssuer());
     HttpArguments httpArguments = service.getRequestParameters(new HashMap<String, String>());
     Assert.assertEquals("https://www.example.com/.well-known/openid-configuration",
         httpArguments.getUrl());
@@ -42,13 +46,59 @@ public class ProviderInfoDiscoveryTest {
   }
 
   @Test
-  public void testSuccessFulIssuerTrailingSlash() throws Exception {
+  public void testHttpParamsSuccessFulIssuerTrailingSlash() throws Exception {
     serviceContext.setIssuer(issuer + "/");
     ProviderInfoDiscovery service = new ProviderInfoDiscovery(serviceContext, null, null);
-    System.out.println(service.getServiceContext().getIssuer());
     HttpArguments httpArguments = service.getRequestParameters(new HashMap<String, String>());
     Assert.assertEquals("https://www.example.com/.well-known/openid-configuration",
         httpArguments.getUrl());
     Assert.assertEquals(HttpMethod.GET, httpArguments.getHttpMethod());
+  }
+  
+  @Test(expected = MissingRequiredAttributeException.class)
+  public void testUpdateCtxMissingIssuer() throws Exception {
+    ProviderInfoDiscovery service = new ProviderInfoDiscovery(serviceContext, null, null);
+    service.updateServiceContext(buildMinimalResponse(issuer));
+  }
+  
+  @Test
+  public void testUpdateCtxSuccess() throws Exception {
+    ProviderInfoDiscovery service = new ProviderInfoDiscovery(serviceContext, null, null);
+    serviceContext.setIssuer(issuer);
+    Assert.assertNull(service.getServiceContext().getProviderConfigurationResponse());
+    service.updateServiceContext(buildMinimalResponse(issuer));
+    ASConfigurationResponse response = service.getServiceContext().getProviderConfigurationResponse();
+    Assert.assertNotNull(response);
+    Assert.assertTrue(response.verify());
+  }
+
+  @Test
+  public void testUpdateCtxSuccessMismatchAllowed() throws Exception {
+    ProviderInfoDiscovery service = new ProviderInfoDiscovery(serviceContext, null, null);
+    serviceContext.setIssuer("https://www.example.org");
+    Map<String, Boolean> allow = new HashMap<String, Boolean>();
+    allow.put(Constants.ALLOW_PARAM_ISSUER_MISMATCH, Boolean.TRUE);
+    serviceContext.setAllow(allow);
+    Assert.assertNull(service.getServiceContext().getProviderConfigurationResponse());
+    service.updateServiceContext(buildMinimalResponse(issuer));
+    ASConfigurationResponse response = service.getServiceContext().getProviderConfigurationResponse();
+    Assert.assertNotNull(response);
+    Assert.assertTrue(response.verify());
+  }
+
+  @Test(expected = InvalidClaimException.class)
+  public void testUpdateCtxFailedMismatch() throws Exception {
+    ProviderInfoDiscovery service = new ProviderInfoDiscovery(serviceContext, null, null);
+    serviceContext.setIssuer("https://www.example.org");
+    Assert.assertNull(service.getServiceContext().getProviderConfigurationResponse());
+    service.updateServiceContext(buildMinimalResponse(issuer + "/"));
+  }
+  
+  protected ASConfigurationResponse buildMinimalResponse(String issuer) {
+    ASConfigurationResponse response = new ASConfigurationResponse();
+    response.addClaim("issuer", issuer);
+    response.addClaim("response_types_supported", Arrays.asList("code"));
+    response.addClaim("grant_types_supported", Arrays.asList("authorization_code"));
+    return response;
   }
 }
