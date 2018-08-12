30f3c81d5e2ca8e84804f457cd207908d849cec5
janne lauros <janne.lauros@gmail.com>
2018-08-12 14:37:50 +0300

    use pre constructor PickRedirectUri

--- src/main/java/org/oidc/service/oidc/Authentication.java
+++ src/main/java/org/oidc/service/oidc/Authentication.java
@@ -18,10 +18,11 @@ package org.oidc.service.oidc;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.oidc.common.EndpointName;
-import org.oidc.common.HttpMethod;
 import org.oidc.common.MissingRequiredAttributeException;
 import org.oidc.common.ServiceName;
 import org.oidc.common.UnsupportedSerializationTypeException;
@@ -36,6 +37,7 @@ import org.oidc.service.base.HttpArguments;
 import org.oidc.service.base.RequestArgumentProcessor;
 import org.oidc.service.base.ServiceConfig;
 import org.oidc.service.base.ServiceContext;
+import org.oidc.service.base.processor.PickRedirectUri;
 import org.oidc.service.data.State;
 
 public class Authentication extends AbstractService {
@@ -48,27 +50,15 @@ public class Authentication extends AbstractService {
     this.responseMessage = new AuthenticationResponse();
     this.expectedResponseClass = AuthenticationResponse.class;
 
-    // TODO : PRECONSTRUCTS & INIT
-    /*
-     * Set default scope value, this is set if not found in gather_request_args in the base class
-     * self.default_request_args = {'scope': ['openid']}
-     * 
-     * // Preconstruct methods 1) set_state.. get state from "extra args"(?), if not there, from req
-     * arqs.. if not there generate 2) see def pick_redirect_uris(request_args=None, service=None,
-     * **kwargs): 3) see def oidc_pre_construct(self, request_args=None, **kwargs):
-     * 
-     * self.pre_construct = [self.set_state, pick_redirect_uris, self.oidc_pre_construct]
-     * 
-     * //PostConstruct methods 1) def oidc_post_construct(self, req, **kwargs)
-     */
-    this.preConstructors = new ArrayList<RequestArgumentProcessor>();
+    this.preConstructors = (List<RequestArgumentProcessor>) Arrays
+        .asList((RequestArgumentProcessor) new PickRedirectUri());
     this.postConstructors = new ArrayList<RequestArgumentProcessor>();
   }
 
   @Override
   protected void doUpdateServiceContext(Message response, String stateKey)
       throws MissingRequiredAttributeException, ValueException, InvalidClaimException {
-    //TODO
+    // TODO
   }
 
   public HttpArguments finalizeGetRequestParameters(HttpArguments httpArguments,
