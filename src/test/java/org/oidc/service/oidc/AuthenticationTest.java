19e91b93525f286943f681dddb9cfbfa2bf5140c
janne lauros <janne.lauros@gmail.com>
2018-08-16 16:51:01 +0300

    initializes now valid request by default

--- src/test/java/org/oidc/service/oidc/AuthenticationTest.java
+++ src/test/java/org/oidc/service/oidc/AuthenticationTest.java
@@ -36,40 +36,44 @@ public class AuthenticationTest extends BaseServiceTest<Authentication> {
 
   ServiceContext serviceContext;
   String issuer;
+  Map<String, Object> map = new HashMap<String, Object>();
 
   @Before
   public void init() {
     serviceContext = new ServiceContext();
     service = new Authentication(serviceContext, null, null);
+    service.setEndpoint("https://www.example.com/authorize");
     List<String> redirectUris = new ArrayList<String>();
     redirectUris.add("https://example.com/cb");
     serviceContext.setRedirectUris(redirectUris);
     issuer = "https://www.example.com";
     serviceContext.setIssuer(issuer);
+    map.clear();
+    map.put("response_type", "code");
+    map.put("scope", "openid");
+    map.put("client_id", "clientid_x");
   }
 
   @Test(expected = MissingRequiredAttributeException.class)
   public void testHttpGetParametersMissingEndpoint() throws Exception {
+    service.setEndpoint(null);
     service.getRequestParameters(new HashMap<String, Object>());
   }
 
   @Test
   public void testHttpGetParametersMinimal() throws Exception {
-    Map<String, Object> map = new HashMap<String, Object>();
-    map.put("response_type", "code");
-    service.setEndpoint("https://www.example.com/authorize");
     HttpArguments httpArguments = service.getRequestParameters(map);
     Assert.assertEquals(HttpMethod.GET, httpArguments.getHttpMethod());
     Assert.assertTrue(httpArguments.getUrl().startsWith("https://www.example.com/authorize"));
+    Assert.assertTrue(httpArguments.getUrl().contains("client_id=clientid_x"));
     Assert.assertTrue(httpArguments.getUrl().contains("scope=openid"));
     Assert.assertTrue(httpArguments.getUrl().contains("response_type=code"));
     Assert
         .assertTrue(httpArguments.getUrl().contains("redirect_uri=https%3A%2F%2Fexample.com%2Fcb"));
   }
-
+  
   @Test
   public void testHttpPostParameters() throws Exception {
-    service.setEndpoint("https://www.example.com/authorize");
     Map<String, Object> requestParameters = new HashMap<String, Object>();
     HttpMethod httpMethod = HttpMethod.POST;
     requestParameters.put("httpMethod", httpMethod);
