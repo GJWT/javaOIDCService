58a240867a399a7b3017ca70fa24fa4816c7c8a4
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-08-18 16:22:10 +0300

    Improved testing

--- src/test/java/org/oidc/service/base/ServiceConfigTest.java
+++ src/test/java/org/oidc/service/base/ServiceConfigTest.java
@@ -19,33 +19,43 @@ package org.oidc.service.base;
 import java.util.Properties;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.oidc.common.ClientAuthenticationMethod;
+import org.oidc.common.HttpMethod;
+import org.oidc.common.SerializationType;
+import org.oidc.msg.DeserializationException;
+import org.oidc.msg.SerializationException;
 import org.oidc.service.util.Constants;
 
 /**
  * Unit tests for {@link ServiceConfig}.
  */
 public class ServiceConfigTest {
-
+  
+  Properties properties;
+  
+  @Before
+  public void setup() {
+    properties = new Properties();
+  }
+  
   @Test(expected = InvalidConfigurationPropertyException.class)
   public void testInvalidPreConstructorsNotExist() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_PRE_CONSTRUCTORS,
         "org.oidc.service.base.processor.NotExistingClass");
-    ServiceConfig config = new ServiceConfig(properties);
+    new ServiceConfig(properties);
   }
 
   @Test(expected = InvalidConfigurationPropertyException.class)
   public void testInvalidPreConstructorsNotCompatible() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_PRE_CONSTRUCTORS,
         "org.oidc.service.base.ServiceConfig");
-    ServiceConfig config = new ServiceConfig(properties);
+    new ServiceConfig(properties);
   }
 
   @Test
   public void testValidPreConstructorsOne() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_PRE_CONSTRUCTORS,
         "org.oidc.service.base.processor.AddClientBehaviourPreference");
     ServiceConfig config = new ServiceConfig(properties);
@@ -54,7 +64,6 @@ public class ServiceConfigTest {
 
   @Test
   public void testValidPreConstructorsTwo() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_PRE_CONSTRUCTORS,
         "org.oidc.service.base.processor.AddClientBehaviourPreference org.oidc.service.base.processor.AddJwksUriOrJwks");
     ServiceConfig config = new ServiceConfig(properties);
@@ -63,23 +72,20 @@ public class ServiceConfigTest {
 
   @Test(expected = InvalidConfigurationPropertyException.class)
   public void testInvalidPostConstructorsNotExist() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_POST_CONSTRUCTORS,
         "org.oidc.service.base.processor.NotExistingClass");
-    ServiceConfig config = new ServiceConfig(properties);
+    new ServiceConfig(properties);
   }
 
   @Test(expected = InvalidConfigurationPropertyException.class)
   public void testInvalidPostConstructorsNotCompatible() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_POST_CONSTRUCTORS,
         "org.oidc.service.base.ServiceConfig");
-    ServiceConfig config = new ServiceConfig(properties);
+    new ServiceConfig(properties);
   }
 
   @Test
   public void testValidPostConstructorsOne() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_POST_CONSTRUCTORS,
         "org.oidc.service.base.processor.AddClientBehaviourPreference");
     ServiceConfig config = new ServiceConfig(properties);
@@ -88,11 +94,111 @@ public class ServiceConfigTest {
 
   @Test
   public void testValidPostConstructorsTwo() throws Exception {
-    Properties properties = new Properties();
     properties.setProperty(Constants.SERVICE_CONFIG_KEY_POST_CONSTRUCTORS,
         "org.oidc.service.base.processor.AddClientBehaviourPreference org.oidc.service.base.processor.AddJwksUriOrJwks");
     ServiceConfig config = new ServiceConfig(properties);
     Assert.assertEquals(2, config.getPostConstructors().size());
   }
-
+  
+  @Test
+  public void testValidYamlTwoPostConstructors() throws DeserializationException {
+    String yaml = "---\n" + 
+        "service_post_constructors:\n" + 
+        "- \"org.oidc.service.base.processor.AddClientBehaviourPreference\"\n" + 
+        "- \"org.oidc.service.base.processor.AddJwksUriOrJwks\"\n"; 
+    ServiceConfig config = ServiceConfig.fromYaml(yaml);
+    Assert.assertEquals(2, config.getPostConstructors().size());
+  }
+  
+  @Test(expected = DeserializationException.class)
+  public void testInvalidYamlPostConstructorsString() throws DeserializationException {
+    String yaml = "---\n" + 
+        "service_post_constructors: \"org.oidc.service.base.processor.AddClientBehaviourPreference\"";
+    ServiceConfig.fromYaml(yaml);
+  }
+  
+  @Test
+  public void testValidYamlTwoPreConstructors() throws DeserializationException {
+    String yaml = "---\n" + 
+        "service_pre_constructors:\n" + 
+        "- \"org.oidc.service.base.processor.AddClientBehaviourPreference\"\n" + 
+        "- \"org.oidc.service.base.processor.AddJwksUriOrJwks\"\n"; 
+    ServiceConfig config = ServiceConfig.fromYaml(yaml);
+    Assert.assertEquals(2, config.getPreConstructors().size());
+  }
+  
+  @Test(expected = DeserializationException.class)
+  public void testInvalidYamlPreConstructorsString() throws DeserializationException {
+    String yaml = "---\n" + 
+        "service_pre_constructors: \"org.oidc.service.base.processor.AddClientBehaviourPreference\"";
+    ServiceConfig.fromYaml(yaml);
+  }
+  
+  @Test
+  public void testValidFromYaml() throws DeserializationException {
+    String yaml= "---\n" + 
+        "service_default_authentication_method: \"CLIENT_SECRET_BASIC\"\n" + 
+        "service_deserialization_type: \"JSON\"\n" + 
+        "service_endpoint: \"https://mock.example.org/\"\n" + 
+        "service_http_method: \"GET\"\n" + 
+        "service_serialization_type: \"URL_ENCODED\"\n" + 
+        "service_allow_non_standard_issuer: true\n" + 
+        "service_allow_http: true";
+    ServiceConfig config = ServiceConfig.fromYaml(yaml);
+    Assert.assertEquals(ClientAuthenticationMethod.CLIENT_SECRET_BASIC, config.getDefaultAuthenticationMethod());
+    Assert.assertEquals(SerializationType.JSON, config.getDeSerializationType());
+    Assert.assertEquals("https://mock.example.org/", config.getEndpoint());
+    Assert.assertEquals(HttpMethod.GET, config.getHttpMethod());
+    Assert.assertEquals(SerializationType.URL_ENCODED, config.getSerializationType());
+    Assert.assertTrue(config.isShouldAllowHttp());
+    Assert.assertTrue(config.isShouldAllowNonStandardIssuer());
+  }
+  
+  @Test(expected = DeserializationException.class)
+  public void testInvalidYamlAuthMethod() throws DeserializationException {
+    String yaml= "---\n" + 
+        "service_default_authentication_method: \"CLIENT_SECRET_INVALID\"\n";
+    ServiceConfig.fromYaml(yaml);
+  }
+  
+  @Test(expected = DeserializationException.class)
+  public void testInvalidYamlHttpMethod() throws DeserializationException {
+    String yaml= "---\n" + 
+        "service_http_method: \"INVALID\"\n";
+    ServiceConfig.fromYaml(yaml);
+  }
+  
+  @Test(expected = DeserializationException.class)
+  public void testInvalidYamlSerialization() throws DeserializationException {
+    String yaml= "---\n" + 
+        "service_serialization_type: \"INVALID\"\n";
+    ServiceConfig.fromYaml(yaml);
+  }
+  
+  @Test(expected = DeserializationException.class)
+  public void testInvalidYamlDeserialization() throws DeserializationException {
+    String yaml= "---\n" + 
+        "service_deserialization_type: \"INVALID\"\n";
+    ServiceConfig.fromYaml(yaml);
+  }
+  
+  @Test
+  public void testToYaml() throws SerializationException {
+    ServiceConfig config = new ServiceConfig();
+    config.setDefaultAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
+    config.setDeSerializationType(SerializationType.JSON);
+    config.setEndpoint("https://mock.example.org/");
+    config.setHttpMethod(HttpMethod.GET);
+    config.setSerializationType(SerializationType.JSON);
+    config.setShouldAllowHttp(true);
+    config.setShouldAllowNonStandardIssuer(true);
+    String yaml = config.toYaml();
+    Assert.assertTrue(yaml.contains("service_default_authentication_method: \"CLIENT_SECRET_BASIC\""));
+    Assert.assertTrue(yaml.contains("service_deserialization_type: \"JSON\""));
+    Assert.assertTrue(yaml.contains("service_endpoint: \"https://mock.example.org/\""));
+    Assert.assertTrue(yaml.contains("service_http_method: \"GET\""));
+    Assert.assertTrue(yaml.contains("service_serialization_type: \"JSON\""));
+    Assert.assertTrue(yaml.contains("service_allow_non_standard_issuer: true"));
+    Assert.assertTrue(yaml.contains("service_allow_http: true"));
+  }
 }
