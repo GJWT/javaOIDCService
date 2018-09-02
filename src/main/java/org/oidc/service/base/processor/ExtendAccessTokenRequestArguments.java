8be161f43a363dd875719d5c2d854e6fda46d9eb
janne lauros <janne.lauros@gmail.com>
2018-09-02 19:49:02 +0300

    initial commit

---
+++ src/main/java/org/oidc/service/base/processor/ExtendAccessTokenRequestArguments.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.oidc.service.base.processor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.oidc.common.MessageType;
+import org.oidc.msg.Error;
+import org.oidc.msg.ErrorDetails;
+import org.oidc.msg.ErrorType;
+import org.oidc.msg.ParameterVerification;
+import org.oidc.service.Service;
+import org.oidc.service.base.RequestArgumentProcessingException;
+
+/**
+ * This class extends the access token arguments by arguments located from originating authorization
+ * request and response.
+ */
+public class ExtendAccessTokenRequestArguments extends AbstractRequestArgumentProcessor {
+
+  {
+    paramVerDefs.put("state", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+  }
+
+  {
+    preParamVerDefs.put("state", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+  }
+
+  @Override
+  protected void processVerifiedArguments(Map<String, Object> requestArguments, Service service,
+      Error error) throws RequestArgumentProcessingException {
+
+    String state = null;
+    if (service.getPreConstructorArgs().containsKey("state")) {
+      state = (String) service.getPreConstructorArgs().get("state");
+    } else if (requestArguments.containsKey("state")) {
+      state = (String) requestArguments.get("state");
+    } else {
+      error.getDetails().add(new ErrorDetails("state", ErrorType.MISSING_REQUIRED_VALUE));
+      throw new RequestArgumentProcessingException(error);
+    }
+
+    Map<String, Object> args = new HashMap<String, Object>();
+    service.getState().extendRequestArgs(args, MessageType.AUTHORIZATION_REQUEST, state,
+        new ArrayList<String>(
+            service.getRequestMessage().getParameterVerificationDefinitions().keySet()));
+    service.getState().extendRequestArgs(args, MessageType.AUTHORIZATION_RESPONSE, state,
+        new ArrayList<String>(
+            service.getRequestMessage().getParameterVerificationDefinitions().keySet()));
+    if (!args.containsKey("grant_type")) {
+      args.put("grant_type", "authorization_code");
+    }
+    // TODO: Verify merging the two maps is really the desired functionality. Remove this tag or
+    // correct logic if needed.
+    requestArguments.putAll(args);
+  }
+}
