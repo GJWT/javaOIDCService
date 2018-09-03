3243174366686574da62302bfc414b67ec6ec5f7
janne lauros <janne.lauros@gmail.com>
2018-09-03 09:43:57 +0300

    postparse response and update service context functionalities added

--- src/main/java/org/oidc/service/oidc/AccessToken.java
+++ src/main/java/org/oidc/service/oidc/AccessToken.java
@@ -16,15 +16,21 @@
 
 package org.oidc.service.oidc;
 
+import org.oidc.common.MessageType;
+import org.oidc.common.MissingRequiredAttributeException;
+import org.oidc.common.ValueException;
+import org.oidc.msg.DeserializationException;
+import org.oidc.msg.InvalidClaimException;
 import org.oidc.msg.Message;
 import org.oidc.msg.oidc.AccessTokenRequest;
 import org.oidc.msg.oidc.AccessTokenResponse;
+import org.oidc.msg.oidc.IDToken;
 import org.oidc.service.base.ServiceConfig;
 import org.oidc.service.base.ServiceContext;
 import org.oidc.service.data.State;
 
 /**
- * only started..
+ * OIDC provider access token service.
  */
 public class AccessToken extends org.oidc.service.oauth2.AccessToken {
 
@@ -34,6 +40,35 @@ public class AccessToken extends org.oidc.service.oauth2.AccessToken {
     this.responseMessage = new AccessTokenResponse();
     this.expectedResponseClass = AccessTokenResponse.class;
   }
+  
+  @Override
+  protected void doUpdateServiceContext(Message response, String stateKey)
+      throws MissingRequiredAttributeException, ValueException, InvalidClaimException {
+    if (!(responseMessage instanceof AccessTokenResponse)) {
+      throw new ValueException("response not instance of AccessTokenResponse");
+    }
+    if (responseMessage.getClaims().containsKey("id_token")) {
+      IDToken idToken = new IDToken();
+      try {
+        // ID Token has already been verified in this stage
+        idToken.fromJwt((String) responseMessage.getClaims().get("id_token"), null, null);
+      } catch (DeserializationException e) {
+        throw new InvalidClaimException(String.format("Unable to decode id token '%s'",
+            (String) responseMessage.getClaims().get("id_token")));
+      }
+      if (!stateKey
+          .equals(getState().getStateKeyByNonce((String) idToken.getClaims().get("nonce")))) {
+        throw new ValueException(
+            String.format("nonce '%s' in the id token is not matching state record '%s'",
+                (String) idToken.getClaims().get("nonce"), stateKey));
+      }
+    }
+    if (responseMessage.getClaims().containsKey("expires_in")) {
+      responseMessage.getClaims().put("__expires_at", (System.currentTimeMillis() / 1000)
+          + (long) responseMessage.getClaims().get("expires_in"));
+    }
+    getState().storeItem(response, stateKey, MessageType.TOKEN_RESPONSE);
+  }
 
   @Override
   public Message postParseResponse(Message responseMessage, String stateKey) {
@@ -42,21 +77,20 @@ public class AccessToken extends org.oidc.service.oauth2.AccessToken {
     }
     AccessTokenResponse response = (AccessTokenResponse) responseMessage;
     response.setKeyJar(getServiceContext().getKeyJar());
-    // TODO: We need to have following commented out methods in AccessTokenResponse
-    // response.setIssuer(getServiceContext().getIssuer());
-    // response.setClientId(getServiceContext().getClientId());
-    // response.setSkew(getServiceContext().getClockSkew());
+    response.setIssuer(getServiceContext().getIssuer());
+    response.setClientId(getServiceContext().getClientId());
+    response.setSkew(getServiceContext().getClockSkew());
     if (getServiceContext().getBehavior() != null
         && getServiceContext().getBehavior().getClaims() != null) {
-      // response.setSigAlg((String) getServiceContext().getBehavior().getClaims()
-      // .get("id_token_signed_response_alg"));
-      // response.setEncAlg((String) getServiceContext().getBehavior().getClaims()
-      // .get("id_token_encrypted_response_alg"));
-      // response.setEncEnc((String) getServiceContext().getBehavior().getClaims()
-      // .get("id_token_encrypted_response_enc"));
+      response.setSigAlg((String) getServiceContext().getBehavior().getClaims()
+          .get("id_token_signed_response_alg"));
+      response.setEncAlg((String) getServiceContext().getBehavior().getClaims()
+          .get("id_token_encrypted_response_alg"));
+      response.setEncEnc((String) getServiceContext().getBehavior().getClaims()
+          .get("id_token_encrypted_response_enc"));
     }
     if (getServiceContext().getAllow().containsKey("missing_kid")) {
-      // response.setAllowMissingKid(getServiceContext().getAllow().get("missing_kid"));
+      response.setAllowMissingKid(getServiceContext().getAllow().get("missing_kid"));
     }
     return responseMessage;
   }
