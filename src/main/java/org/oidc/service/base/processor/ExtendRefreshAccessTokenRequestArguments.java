d28007dc541b6095566dcc12dcad8bd96d186f87
janne lauros <janne.lauros@gmail.com>
2018-09-03 10:34:11 +0300

    initial commit

---
+++ src/main/java/org/oidc/service/base/processor/ExtendRefreshAccessTokenRequestArguments.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.oidc.service.base.processor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.oidc.common.MessageType;
+import org.oidc.msg.Error;
+import org.oidc.msg.ErrorDetails;
+import org.oidc.msg.ErrorType;
+import org.oidc.msg.ParameterVerification;
+import org.oidc.service.Service;
+import org.oidc.service.base.RequestArgumentProcessingException;
+
+/**
+ * This class extends the refresh access token arguments by arguments located from access token and
+ * refresh access token responses.
+ */
+public class ExtendRefreshAccessTokenRequestArguments extends AbstractRequestArgumentProcessor {
+
+  {
+    paramVerDefs.put("state", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+  }
+
+  {
+    preParamVerDefs.put("state", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+  }
+
+  @Override
+  protected void processVerifiedArguments(Map<String, Object> requestArguments, Service service,
+      Error error) throws RequestArgumentProcessingException {
+
+    String state = null;
+    if (service.getPreConstructorArgs().containsKey("state")) {
+      state = (String) service.getPreConstructorArgs().get("state");
+    } else if (requestArguments.containsKey("state")) {
+      state = (String) requestArguments.get("state");
+    } else {
+      error.getDetails().add(new ErrorDetails("state", ErrorType.MISSING_REQUIRED_VALUE));
+      throw new RequestArgumentProcessingException(error);
+    }
+
+    Map<String, Object> args = new HashMap<String, Object>();
+    service.getState().extendRequestArgs(args, MessageType.TOKEN_RESPONSE, state,
+        new ArrayList<String>(
+            service.getRequestMessage().getParameterVerificationDefinitions().keySet()));
+    service.getState().extendRequestArgs(args, MessageType.REFRESH_TOKEN_RESPONSE, state,
+        new ArrayList<String>(
+            service.getRequestMessage().getParameterVerificationDefinitions().keySet()));
+    // TODO: Verify merging the two maps is really the desired functionality. Remove this tag or
+    // correct logic if needed.
+    requestArguments.putAll(args);
+  }
+}
---
+++ src/main/java/org/oidc/service/oauth2/RefreshAccessToken.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.oidc.service.oauth2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.oidc.common.ClientAuthenticationMethod;
+import org.oidc.common.EndpointName;
+import org.oidc.common.HttpMethod;
+import org.oidc.common.MessageType;
+import org.oidc.common.MissingRequiredAttributeException;
+import org.oidc.common.SerializationType;
+import org.oidc.common.ServiceName;
+import org.oidc.common.ValueException;
+import org.oidc.msg.InvalidClaimException;
+import org.oidc.msg.Message;
+import org.oidc.msg.oauth2.AccessTokenRequest;
+import org.oidc.msg.oauth2.AccessTokenResponse;
+import org.oidc.msg.oauth2.RefreshAccessTokenRequest;
+import org.oidc.service.AbstractService;
+import org.oidc.service.base.HttpArguments;
+import org.oidc.service.base.RequestArgumentProcessingException;
+import org.oidc.service.base.RequestArgumentProcessor;
+import org.oidc.service.base.ServiceConfig;
+import org.oidc.service.base.ServiceContext;
+import org.oidc.service.base.processor.ExtendRefreshAccessTokenRequestArguments;
+import org.oidc.service.data.State;
+
+/**
+ * OAUTH2 provider refresh access token service.
+ */
+public class RefreshAccessToken extends AbstractService {
+
+  public RefreshAccessToken(ServiceContext serviceContext, State state,
+      ServiceConfig serviceConfig) {
+    super(serviceContext, state, serviceConfig);
+    this.serviceName = ServiceName.REFRESH_ACCESS_TOKEN;
+    this.endpointName = EndpointName.TOKEN;
+    this.requestMessage = new RefreshAccessTokenRequest();
+    this.responseMessage = new AccessTokenResponse();
+    this.isSynchronous = true;
+    this.serializationType = SerializationType.URL_ENCODED;
+    this.deserializationType = SerializationType.JSON;
+    this.httpMethod = HttpMethod.POST;
+    this.expectedResponseClass = AccessTokenResponse.class;
+    this.defaultAuthenticationMethod = ClientAuthenticationMethod.BEARER_HEADER;
+
+    this.preConstructors = (List<RequestArgumentProcessor>) Arrays
+        .asList((RequestArgumentProcessor) new ExtendRefreshAccessTokenRequestArguments());
+
+  }
+
+  @Override
+  protected void doUpdateServiceContext(Message response, String stateKey)
+      throws MissingRequiredAttributeException, ValueException, InvalidClaimException {
+    if (!(responseMessage instanceof AccessTokenResponse)) {
+      throw new ValueException("response not instance of AccessTokenResponse");
+    }
+    if (responseMessage.getClaims().containsKey("expires_in")) {
+      responseMessage.getClaims().put("__expires_at", (System.currentTimeMillis() / 1000)
+          + (long) responseMessage.getClaims().get("expires_in"));
+    }
+    getState().storeItem(response, stateKey, MessageType.TOKEN_RESPONSE);
+  }
+
+  public HttpArguments finalizeGetRequestParameters(HttpArguments httpArguments,
+      Map<String, Object> requestArguments) throws RequestArgumentProcessingException {
+
+    return httpArguments;
+  }
+
+  @Override
+  protected Message doConstructRequest(Map<String, Object> requestArguments)
+      throws RequestArgumentProcessingException {
+    return new AccessTokenRequest(requestArguments);
+  }
+
+}
