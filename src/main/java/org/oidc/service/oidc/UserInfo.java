56a589c2170b064dfb15890c8741f48bba890351
janne lauros <janne.lauros@gmail.com>
2018-09-10 15:00:29 +0300

    initial implementation for postParseResponse added

--- src/main/java/org/oidc/service/oidc/UserInfo.java
+++ src/main/java/org/oidc/service/oidc/UserInfo.java
@@ -17,16 +17,20 @@
 package org.oidc.service.oidc;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.oidc.common.ClientAuthenticationMethod;
 import org.oidc.common.EndpointName;
 import org.oidc.common.HttpMethod;
+import org.oidc.common.MessageType;
 import org.oidc.common.MissingRequiredAttributeException;
 import org.oidc.common.ServiceName;
 import org.oidc.common.ValueException;
+import org.oidc.msg.DeserializationException;
 import org.oidc.msg.InvalidClaimException;
 import org.oidc.msg.Message;
+import org.oidc.msg.oidc.IDToken;
 import org.oidc.msg.oidc.OpenIDSchema;
 import org.oidc.service.AbstractService;
 import org.oidc.service.base.HttpArguments;
@@ -89,9 +93,50 @@ public class UserInfo extends AbstractService {
     if (!(responseMessage instanceof OpenIDSchema)) {
       return responseMessage;
     }
-    /**
-     * TODO
+    // TODO: userinfo response needs to be able to deserialize itself from jwt. Add following
+    // parameters to openid schema.
+    // Then openid schema needs to implement TBD interface with method fromJWT().
+    /*
+     * OpenIDSchema response = (OpenIDSchema) responseMessage;
+     * response.setKeyJar(getServiceContext().getKeyJar());
+     * response.setIssuer(getServiceContext().getIssuer());
+     * response.setClientId(getServiceContext().getClientId());
+     * response.setSkew(getServiceContext().getClockSkew()); if (getServiceContext().getBehavior()
+     * != null && getServiceContext().getBehavior().getClaims() != null) {
+     * response.setSigAlg((String) getServiceContext().getBehavior().getClaims()
+     * .get("userinfo_signed_response_alg")); response.setEncAlg((String)
+     * getServiceContext().getBehavior().getClaims() .get("userinfo_encrypted_response_alg"));
+     * response.setEncEnc((String) getServiceContext().getBehavior().getClaims()
+     * .get("userinfo_encrypted_response_enc")); } if
+     * (getServiceContext().getAllow().containsKey("missing_kid")) {
+     * response.setAllowMissingKid(getServiceContext().getAllow().get("missing_kid")); }
      */
     return responseMessage;
   }
+
+  @Override
+  public Message postParseResponse(Message responseMessage, String stateKey)
+      throws DeserializationException {
+    Map<String, Object> args = new HashMap<String, Object>();
+    getState().multipleExtendRequestArgs(args, stateKey, Arrays.asList("id_token"),
+        Arrays.asList(MessageType.AUTHORIZATION_RESPONSE, MessageType.TOKEN_RESPONSE,
+            MessageType.REFRESH_TOKEN_RESPONSE));
+    if (args.containsKey("id_token")) {
+      IDToken idToken = new IDToken();
+      // ID Token has already been verified in this stage
+      idToken.fromJwt((String) responseMessage.getClaims().get("id_token"), null, null);
+      String expectedSub = (String) responseMessage.getClaims().get("sub");
+      String receivedSub = (String) idToken.getClaims().get("sub");
+      if (expectedSub.equals(receivedSub)) {
+        throw new DeserializationException(String
+            .format("expected sub value '%s' but got instead '%s'", expectedSub, receivedSub));
+      }
+    } else {
+      // TODO: log warning about not being able to verify sub
+    }
+    // TODO: add handling of distributed claims
+    state.storeItem(responseMessage, stateKey, MessageType.USER_INFO);
+    return responseMessage;
+  }
+
 }
