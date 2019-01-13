4fa164a542905e7d48d7410378112ce5157f93fc
janne lauros <janne.lauros@gmail.com>
2019-01-13 15:05:47 +0200

    tests added for more coverage

--- src/test/java/org/oidc/service/util/ServiceUtilTest.java
+++ src/test/java/org/oidc/service/util/ServiceUtilTest.java
@@ -19,6 +19,8 @@ package org.oidc.service.util;
 import static org.hamcrest.core.StringContains.containsString;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Assert;
@@ -27,7 +29,9 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.oidc.common.SerializationType;
 import org.oidc.common.UnsupportedSerializationTypeException;
+import org.oidc.common.ValueException;
 import org.oidc.msg.AbstractMessage;
+import org.oidc.msg.DeserializationException;
 import org.oidc.msg.InvalidClaimException;
 import org.oidc.msg.Message;
 import org.oidc.msg.SerializationException;
@@ -99,6 +103,61 @@ public class ServiceUtilTest {
     claims.put(Constants.ISSUER, "issuer");
     Message request = new MockMessage(claims);
     ServiceUtil.getHttpBody(request, SerializationType.JWT);
+  }
+
+  @Test
+  public void testnullOrEmptyStringOrList() {
+    Assert.assertTrue(ServiceUtil.nullOrEmptyStringOrList(null));
+    Assert.assertTrue(ServiceUtil.nullOrEmptyStringOrList(""));
+    Assert.assertTrue(ServiceUtil.nullOrEmptyStringOrList(new ArrayList<String>()));
+    Assert.assertFalse(ServiceUtil.nullOrEmptyStringOrList(new HashMap<String, String>()));
+  }
+
+  @SuppressWarnings("rawtypes")
+  @Test
+  public void testparseJsonFileToMapSuccess() throws DeserializationException {
+    Map jwkMap = ServiceUtil.parseJsonFileToMap("src/test/resources/jwk.json");
+    Assert.assertNotNull(jwkMap);
+    ArrayList keys = (ArrayList) jwkMap.get("keys");
+    Assert.assertTrue(((Map) keys.get(0)).containsKey("alg"));
+    Assert.assertTrue(((Map) keys.get(0)).containsKey("kid"));
+    Assert.assertTrue(((Map) keys.get(0)).containsKey("kty"));
+    Assert.assertTrue(((Map) keys.get(0)).containsKey("use"));
+    Assert.assertTrue(((Map) keys.get(0)).containsKey("n"));
+  }
+
+  @Test(expected = DeserializationException.class)
+  public void testparseJsonFileToMapFailure() throws DeserializationException {
+    ServiceUtil.parseJsonFileToMap("src/test/resources/rsa-private.pem");
+  }
+
+  @Test
+  public void testalgorithmToKeytypeForJWS() {
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("none"), "none");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("RS256"), "RSA");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("RS384"), "RSA");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("RS512"), "RSA");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("PS256"), "RSA");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("PS384"), "RSA");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("PS512"), "RSA");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("ES256"), "EC");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("ES384"), "EC");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("ES512"), "EC");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("HS256"), "oct");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("HS384"), "oct");
+    Assert.assertEquals(ServiceUtil.algorithmToKeytypeForJWS("HS512"), "oct");
+  }
+
+  @Test
+  public void testgetFilenameFromWebnameSuccess() throws ValueException {
+    Assert.assertEquals(ServiceUtil.getFilenameFromWebname("http://example.com",
+        "http://example.com/filesdir/filename"), "filesdir/filename");
+  }
+
+  @Test(expected = ValueException.class)
+  public void testgetFilenameFromWebnameFailure() throws ValueException {
+    ServiceUtil.getFilenameFromWebname("http://example.com",
+        "http://someelse.com/filesdir/filename");
   }
 
   class MockMessage extends AbstractMessage {
