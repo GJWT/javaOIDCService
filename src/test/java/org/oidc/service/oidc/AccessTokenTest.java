4a07816ca714134b6209ab3e1e71142bb97007d6
janne lauros <janne.lauros@gmail.com>
2019-01-23 09:00:35 +0200

    nonve mismatch tested

--- src/test/java/org/oidc/service/oidc/AccessTokenTest.java
+++ src/test/java/org/oidc/service/oidc/AccessTokenTest.java
@@ -43,7 +43,6 @@ import org.oidc.service.base.ServiceContext;
 import org.oidc.service.data.InMemoryStateImpl;
 import org.oidc.service.data.State;
 
-
 /**
  * Unit tests for {@link AccessToken}.
  */
@@ -78,8 +77,8 @@ public class AccessTokenTest extends BaseServiceTest<AccessToken> {
     serviceContext.getBehavior().getClaims().put("id_token_encrypted_response_enc", "A128GCM");
     serviceContext.getBehavior().getClaims().put("token_endpoint_auth_method", "bearer_body");
     serviceContext.getAllow().put("missing_kid", true);
-    
-    AuthenticationResponse resp=new AuthenticationResponse();
+
+    AuthenticationResponse resp = new AuthenticationResponse();
     resp.addClaim("code", "authorizationcode");
     state.storeItem(resp, stateKey, MessageType.AUTHORIZATION_RESPONSE);
     Map<String, Object> preConstructorArgs = new HashMap<String, Object>();
@@ -95,7 +94,7 @@ public class AccessTokenTest extends BaseServiceTest<AccessToken> {
     Assert.assertTrue(httpArguments.getBody().contains("code=authorizationcode"));
     Assert.assertTrue(httpArguments.getBody().contains("grant_type=authorization_code"));
   }
-  
+
   @Test
   public void testdoUpdateServiceContextResponseStored()
       throws MissingRequiredAttributeException, ValueException, InvalidClaimException {
@@ -108,16 +107,8 @@ public class AccessTokenTest extends BaseServiceTest<AccessToken> {
     Assert.assertEquals("value", response.getClaims().get("access_token"));
   }
 
-  @Test
-  public void testdoUpdateServiceContextIdTokenStored() throws MissingRequiredAttributeException,
-      ValueException, InvalidClaimException, SerializationException {
-    // We have to create a id token that passes the tests
-    AccessTokenResponse response = new AccessTokenResponse();
-    response.addClaim("token_type", "bearer");
-    response.addClaim("access_token", "value");
+  private IDToken getIDToken() {
     Date now = new Date();
-    response.setSigAlg("none");
-    response.setClientId(clientId);
     IDToken idToken = new IDToken();
     idToken.addClaim("iss", issuer);
     idToken.addClaim("sub", "user01");
@@ -125,8 +116,42 @@ public class AccessTokenTest extends BaseServiceTest<AccessToken> {
     idToken.addClaim("exp", new Date(now.getTime() + 1000));
     idToken.addClaim("iat", now);
     idToken.addClaim("nonce", "noncevalue");
-    response.addClaim("id_token", idToken.toJwt(null, "none", null, null, null, null, null, null));
+    return idToken;
+  }
+
+  @Test
+  public void testdoUpdateServiceContextIdTokenStored() throws MissingRequiredAttributeException,
+      ValueException, InvalidClaimException, SerializationException {
+    // We have to create a id token that passes the tests
+    AccessTokenResponse response = new AccessTokenResponse();
+    response.addClaim("token_type", "bearer");
+    response.addClaim("access_token", "value");
+    response.setSigAlg("none");
+    response.setClientId(clientId);
+    response.addClaim("id_token",
+        getIDToken().toJwt(null, "none", null, null, null, null, null, null));
     state.storeStateKeyForNonce("noncevalue", stateKey);
+    service.updateServiceContext(response, stateKey);
+    Message storedIDToken = state.getItem(stateKey, MessageType.VERIFIED_IDTOKEN);
+    Message storedResponse = state.getItem(stateKey, MessageType.TOKEN_RESPONSE);
+    Assert.assertNotNull(((AccessTokenResponse) storedResponse).getVerifiedIdToken());
+    Assert.assertTrue(storedIDToken instanceof IDToken);
+    Assert.assertEquals("noncevalue", storedIDToken.getClaims().get("nonce"));
+  }
+
+  @Test(expected = InvalidClaimException.class)
+  public void testdoUpdateServiceContextIdTokenStoredNonceFail()
+      throws MissingRequiredAttributeException, ValueException, InvalidClaimException,
+      SerializationException {
+    // We have to create a id token that passes the tests
+    AccessTokenResponse response = new AccessTokenResponse();
+    response.addClaim("token_type", "bearer");
+    response.addClaim("access_token", "value");
+    response.setSigAlg("none");
+    response.setClientId(clientId);
+    response.addClaim("id_token",
+        getIDToken().toJwt(null, "none", null, null, null, null, null, null));
+    state.storeStateKeyForNonce("noncevalueWrong", stateKey);
     service.updateServiceContext(response, stateKey);
     Message storedIDToken = state.getItem(stateKey, MessageType.VERIFIED_IDTOKEN);
     Message storedResponse = state.getItem(stateKey, MessageType.TOKEN_RESPONSE);
@@ -148,41 +173,30 @@ public class AccessTokenTest extends BaseServiceTest<AccessToken> {
     Assert.assertTrue(storedResponse instanceof AccessTokenResponse);
     Assert.assertTrue(response.getClaims().containsKey("__expires_at"));
   }
-  
-  //@Test (expected=InvalidClaimException.class)
-  public void testdoUpdateServiceContextWrongStatekey()
-      throws MissingRequiredAttributeException, ValueException, InvalidClaimException {
-    AccessTokenResponse response = new AccessTokenResponse();
-    response.addClaim("token_type", "bearer");
-    response.addClaim("access_token", "value");
-    long expires = 3600;
-    response.addClaim("expires_in", expires);
-    service.updateServiceContext(response, "WrongStateKey");
-  }
 
   @Test
   public void testprepareMessageForVerification() {
     AccessTokenResponse response = new AccessTokenResponse();
     service.prepareMessageForVerification(response);
-    //There is no simple way to test parameters are set except cobertura report
+    // There is no simple way to test parameters are set except cobertura report
   }
-  
+
   @Test
   public void testprepareMessageForVerificationNullInput() {
     AccessTokenResponse response = new AccessTokenResponse();
     serviceContext.setBehavior(null);
     serviceContext.getAllow().put("missing_kid", null);
     service.prepareMessageForVerification(response);
-    //There is no simple way to test parameters are set except cobertura report
+    // There is no simple way to test parameters are set except cobertura report
   }
-  
+
   @Test
   public void testgetDefaultAuthenticationMethodNoInput() {
     serviceContext.getBehavior().getClaims().remove("token_endpoint_auth_method");
     Assert.assertEquals(ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
         service.getDefaultAuthenticationMethod());
   }
-  
+
   @Test
   public void testgetDefaultAuthenticationMethodPreferredSet() {
     Assert.assertEquals(ClientAuthenticationMethod.BEARER_BODY,
